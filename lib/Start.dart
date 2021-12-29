dc30ac3ec18336f7c1a796730ec3c7fe6715688c
BhanudasRane1 <ranebhanudas786@gmail.com>
2021-12-30 00:26:26 +0530

    first commit

---
+++ lib/Start.dart
@@ -0,0 +1,103 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:erpapp/Login.dart';
+import 'SignUp.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class Start extends StatefulWidget {
+  @override
+  _StartState createState() => _StartState();
+}
+
+class _StartState extends State<Start> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  navigateToLogin() async {
+    Navigator.pushReplacementNamed(context, "Login");
+  }
+
+  navigateToRegister() async {
+    Navigator.pushReplacementNamed(context, "SignUp");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("MVPs")),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            // SizedBox(height: 35.0),
+            Container(
+              // height: 400,
+              child: Image(
+                image: AssetImage("images/start_page.jpeg"),
+                fit: BoxFit.contain,
+              ),
+            ),
+            SizedBox(height: 20),
+            RichText(
+                text: TextSpan(
+                    text: 'Welcome to ',
+                    style: TextStyle(
+                        fontSize: 25.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black),
+                    children: <TextSpan>[
+                  TextSpan(
+                      text: 'KBTCOE NASHIK',
+                      style: TextStyle(
+                          fontSize: 30.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.orange))
+                ])),
+            SizedBox(height: 15.0),
+            Text(
+              'SMART WORLD , SMART CAMPUS',
+              style: TextStyle(color: Colors.black),
+            ),
+            SizedBox(height: 50.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                RaisedButton(
+                    padding: EdgeInsets.only(left: 30, right: 30),
+                    onPressed: navigateToLogin,
+                    child: Text(
+                      'LOGIN',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    color: Colors.orange),
+                SizedBox(width: 20.0),
+                RaisedButton(
+                    padding: EdgeInsets.only(left: 30, right: 30),
+                    onPressed: navigateToRegister,
+                    child: Text(
+                      'REGISTER',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    color: Colors.orange),
+              ],
+            ),
+            SizedBox(height: 20.0),
+          ],
+        ),
+      ),
+    );
+  }
+}
